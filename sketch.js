13f11e43d2dcf4efc7e71bb3119e1859fafd6252
ylan0208 <167422318+ylan0208@users.noreply.github.com>
2024-05-30 09:48:47 +0800

    edit group code
    changed original group code: renamed one function name, created a new class, tried to improve responsive window

--- sketch.js
+++ sketch.js
@@ -1,29 +1,101 @@
+let lineSettings;
+let lines = []//array
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);//Create a canvas
-  background(246, 240, 221);//background color
-  createLines();
+  createCanvas(windowWidth, windowHeight);//Create a canvas of windowsize
+  setLines();
 }
 
-function draw()
-{
-  
+function draw(){
+  background(246, 240, 221);//set background color as paper yellow
+  for (let l of lines){
+    l.display();
+  }
 }
 
-function windowResized()
-{
-  //React to window size
+function windowResized(){
+  //when window size is changed, adjust canvas
   resizeCanvas(windowWidth, windowHeight);
-  background(246, 240, 221);
-  createLines();
 }
 
+class Line {
+  constructor(tx, ty, leng, xOff, ys, ang, color, sw) {
 
-function createLines()
-{
+    this.moveX = 0;
+   
+    this.initData = {
+      tx: tx,
+      ty: ty,
+      leng: leng, //length
+      xOff: xOff,
+      ys: ys, //y scale
+      ang: ang, // angle
+      color: color,
+      sw: sw, //strokeweight
+    }
+    //save initial properties for later animation
+    this.init()
+  }
+
+  init() {
+    //initialise line properties
+    this.tx = this.initData.tx;
+    this.ty = this.initData.ty;
+    this.xOff = this.initData.xOff;
+    this.ys = this.initData.ys;
+    this.leng = this.initData.leng;
+    this.ang = this.initData.ang;
+    this.color = this.initData.color;
+    this.sw = this.initData.sw;
+    
+    let xOff = this.xOff * width;
+    let leng = this.leng * width;
+    this.yPos = width * this.ys;
+    //calculate x y position of 2 endpoints of each line
+    this.x1 = -leng / 2 + xOff + this.moveX
+    this.y1 = this.yPos
+    this.x2 = leng / 2 + xOff + this.moveX
+    this.y2 = this.yPos
+
+  }
+
+
+  display() {
+
+    let xOff = this.xOff * width;
+    let leng = this.leng * width;
+    this.yPos = width * this.ys;
+    this.x1 = lerp(this.x1, -leng / 2 + xOff + this.moveX, 0.09)
+    this.x2 = lerp(this.x2, leng / 2 + xOff + this.moveX, 0.09)
+    this.y1 = lerp(this.y1, this.yPos, 0.09)
+    this.y2 = lerp(this.y2, this.yPos, 0.09)
+
+    //draw the line
+    push();
+    stroke(this.color);
+    strokeWeight(this.sw);
+    translate(this.tx * width, this.ty * width);
+    rotate(this.ang);
+    line(this.x1, this.y1, this.x2, this.y2);
+    pop();
+  }
+}
+
+function drawLines({ x, y, angle, color, weight, lengthLeft, lengthRight, distanceStart, distanceEnd, num}) {
+  
+  for (let i = 0; i <= num; i++) {//Draw number of the lines
+    let xOff = map(i, 0, num, -0.03, 0.01);//line position on the x
+    strokeWeight(weight);//line weight
+    let length = map(i, 0, num, lengthLeft, lengthRight);//line length
+    let yOff = map(i, 0, num, distanceStart, distanceEnd);//line position on the y
+    let l = new Line(x, y, length, xOff, yOff, angle, color, weight)
+    lines.push(l)
+  }
+}
+
+function setLines(){
   strokeCap(SQUARE);
-  //scale the canvas.
-  let scaleFactor = min(windowWidth/1920, windowHeight/1080);
-  scale(scaleFactor);
+  
   
   //radians angle mode value
   let lineSettings = [//array for each set of lines
@@ -43,23 +115,8 @@ function createLines()
     { x: 0.48, y: 0.45, angle: -0.58, color: 0, weight: 1, lengthLeft: 0.53, lengthRight: 0.53, distanceStart: -0.13, distanceEnd: -0.11, num: 1 },
     { x: 0.45, y: 0.64, angle: -0.58, color: 0, weight: 6, lengthLeft: 0.20, lengthRight: 0.20, distanceStart: -0.15, distanceEnd: -0.15, num: 1},
     { x: 0.30, y: 0.56, angle: -0.58, color: 0, weight: 4, lengthLeft: 0.20, lengthRight: 0.20, distanceStart: -0.15, distanceEnd: -0.08, num: 1},
-  ];
-  for (let settings of lineSettings) {//Iteration
+  ];for (let settings of lineSettings){//Iteration
     drawLines(settings);//Call drawLines() to draw the lines
   }
 }
 
-function drawLines({ x, y, angle, color, weight, lengthLeft, lengthRight, distanceStart, distanceEnd, num}) {
-  push();
-  translate(width * x, width * y);//Move the origin to (x, y).
-  rotate(angle);//Rotate angle
-  stroke(color);//line color
-  for (let i = 0; i <= num; i++) {//Draw number of the lines
-    let xOff = map(i, 0, num, -width * 0.03, width * 0.01);//line position on the x
-    strokeWeight(weight);//line weight
-    let length = map(i, 0, num, width * lengthLeft, width * lengthRight);//line length
-    let yOff = map(i, 0, num, width * distanceStart, width * distanceEnd);//line position on the y
-    line(-length / 2 + xOff, yOff, length / 2 + xOff, yOff);//Draw the line
-  }
-  pop();
-}
